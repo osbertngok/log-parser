255b933c18715942363cd9d2e147a98d3d067309
Osbert Ngok <osbertngok@gmail.com>
2018-05-06 20:35:00 +0800

    append to existing json

--- models/gen.go
+++ models/gen.go
@@ -6,9 +6,26 @@ import (
 	"github.com/osbertngok/log-parser/parsergen"
 	"os"
 	"encoding/json"
+	"io/ioutil"
 )
 
+func loadTable(t *parsergen.Table) bool {
+	// load dict.json if exists
+	raw, err := ioutil.ReadFile("./dict.json")
+	if err != nil {
+		// it is fine if the file doesn't exist
+		// we simply won't load it
+		return false
+	}
+
+	err = json.Unmarshal(raw, t)
+	return err == nil
+}
+
 func main() {
+	var initT *parsergen.Table = nil
+	loadTable(initT)
+
 	wf, err := os.Create("./dict.json")
 	if err != nil {
 		panic(err)
